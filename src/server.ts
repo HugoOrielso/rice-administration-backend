15574d9146a546270afe244318f116511b77d1f0
Hugo Orielso <orielso.lozano15@gmail.com>
2026-03-31 03:05:45 -0500

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 // src/app.ts
 import express from "express";
 import authRouter from "./router/auth.routes";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  credentials: true,
+}));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 
 app.use("/api/auth", authRouter);
 
