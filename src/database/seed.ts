ceaaa28fbd77761333a124704bcaccdd167997f3
Hugo Orielso <orielso.lozano15@gmail.com>
2026-04-08 12:06:27 -0500

    seed changed

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -4,25 +4,40 @@ import bcrypt from "bcrypt";
 import { prisma } from "./db";
 
 export async function runSeed() {
-  const adminExists = await prisma.user.findUnique({
-    where: { email: "admin@arrozzulia.com" },
-  });
-
-  if (adminExists) {
-    return;
-  }
-
-  const hashedPassword = await bcrypt.hash("12345678", 10);
-
-  await prisma.user.create({
-    data: {
+  const users = [
+    {
       name: "Administrador",
       email: "admin@arrozzulia.com",
-      password: hashedPassword,
+      password: "&4kBa$gMyPSFVbCJf3yP!H",
       role: "ADMIN",
-      isActive: true,
     },
-  });
+    {
+      name: "Auxiliar",
+      email: "auxiliar@arrozzulia.com",
+      password: "f#FLKGtEe2595ZDi6D6RM%",
+      role: "OPERATOR",
+    },
+  ];
+
+  for (const user of users) {
+    const exists = await prisma.user.findUnique({
+      where: { email: user.email },
+    });
+
+    if (exists) continue;
+
+    const hashedPassword = await bcrypt.hash(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: hashedPassword,
+        role: user.role as "ADMIN" | "OPERATOR",
+        isActive: true,
+      },
+    });
+  }
 
   console.log("Seed ejecutado correctamente 🚀");
 }
